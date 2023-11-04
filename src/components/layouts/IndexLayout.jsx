76b5dc8e3ae42327d580945dacb51e530b198048
baehaky <baehaky1504@gmail.com>
2023-11-04 23:41:33 +0800

    modified:   src/components/layouts/IndexLayout.jsx

--- src/components/layouts/IndexLayout.jsx
+++ src/components/layouts/IndexLayout.jsx
@@ -14,8 +14,8 @@ const IndexLayout = () => {
             animate={{ opacity: 1, scale: 1 }}
             transition={{
               duration: 0.8,
-              delay: 0.5,
-              ease: [0, 0.71, 0.2, 1.01],
+              delay: 1,
+              ease: [0, 0.71, 0.3, 1.01],
             }}
             className="text-white font-bold lg:text-5xl sm:text-4xl text-2xl text-center"
           >
